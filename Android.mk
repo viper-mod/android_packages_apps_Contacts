9de9c74e813d2caf12a2535e842019a41fac0deb
sdhz150 <yanghui150@163.com>
2018-09-15 16:01:39 +0800

    Allow using private framework API.
    Change-Id: I18ed1100b6e25af0b9c76f107e04d306f07c7f81

--- Android.mk
+++ Android.mk
@@ -27,6 +27,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-common \
     com.android.vcard \
     guava \
+    voip-common \
     libphonenumber
 
 LOCAL_USE_AAPT2 := true
@@ -41,7 +42,8 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_SDK_VERSION := current
+#LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_MIN_SDK_VERSION := 21
 
 include $(BUILD_PACKAGE)
